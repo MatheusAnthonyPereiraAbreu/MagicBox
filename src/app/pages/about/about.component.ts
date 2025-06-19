12b8305942ddfdeb0fe88fc08fb4e52fc3ce091e
Matheus Anthony <matheus.anthony.pa@outlook.com>
2025-06-19 18:39:33 -0300

    feat:(V2) adição do novo ícone do projeto, integração com a API do git hub, e adição do conteúdo no about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AboutService, GithubUser } from './about.service';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
-  template: `
-        <div class="p-6">
-            <h1 class="text-3xl font-bold text-text-heading mb-4">Sobre</h1>
-            <p class="text-text-primary">Página sobre o PurpleBox.</p>
-        </div>
-    `,
-  standalone: true
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+  providers: [AboutService]
 })
-export class AboutComponent { }
+export class AboutComponent implements OnInit {
+  users: { user: GithubUser | null, role: string }[] = [
+    { user: null, role: 'DataBase' },
+    { user: null, role: 'Documentador' },
+    { user: null, role: 'Front-End' },
+    { user: null, role: 'Engenheiro de Software' }
+  ];
+  usernames = ['tomlavez', 'Lucasskyher', 'MatheusAnthonyPereiraAbreu', 'AugustoZanoli'];
+
+  constructor(private aboutService: AboutService) { }
+
+  ngOnInit() {
+    this.usernames.forEach((username, idx) => {
+      this.aboutService.getUser(username).subscribe(user => {
+        this.users[idx].user = user;
+      });
+    });
+  }
+
+  openProfile(url?: string) {
+    if (url) {
+      window.open(url, '_blank');
+    }
+  }
+}
