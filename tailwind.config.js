3fffaa3cff3e2b43a0a097078cc0349f996f6267
Matheus Anthony <matheus.anthony.pa@outlook.com>
2025-06-19 17:53:03 -0300

    feat:(V2) Atualização das cores e home.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         primary: {
           light: '#9B59B6',    // Lilás Profundo
           DEFAULT: '#8E44AD',  // Roxo Vibrante
-          dark: '#5B2C6F',     // Roxo Escuro
+          dark: '#6D28D9',     // Roxo Escuro (purple-800)
           darker: '#4A235A',   // Ametista
         },
         secondary: {
@@ -21,7 +21,8 @@ module.exports = {
           primary: '#2C2C34',    // Para textos principais
           secondary: '#000000',   // Para textos secundários
           heading: '#9B59B6',    // Para títulos
-          highlight: '#9B59B6'    // Para destaques
+          highlight: '#9B59B6',    // Para destaques
+          dark: '#6D28D9'
         },
         background: {
           primary: '#4A235A',    // Fundo principal
