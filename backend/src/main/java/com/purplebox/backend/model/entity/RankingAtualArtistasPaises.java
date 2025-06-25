c87871d5739dbfe592975cefb03d0489037a068f
typeDefAlejandro <typedefalejandro@gmail.com>
2025-06-25 20:20:37 -0300

    Adicionado o BackEnd

---
+++ backend/src/main/java/com/purplebox/backend/model/entity/RankingAtualArtistasPaises.java
@@ -0,0 +1,39 @@
+package com.purplebox.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "ranking_atual_artistas_paises")
+public class RankingAtualArtistasPaises {
+
+    @EmbeddedId
+    private RankingAtualArtistasPaisesId id;
+
+    /** posição do artista no país */
+    private Short posicaoRanking;
+
+    private LocalDateTime dataUltimaAtualizacao;
+
+    /* ===================== Relaciones ===================== */
+    @MapsId("artistaId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artista_id", nullable = false)
+    private Artista artista;
+
+    @MapsId("paisId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pais_id", nullable = false)
+    private Pais pais;
+}
